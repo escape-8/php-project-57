b8c66f58a8a8f638bba61926453ad96c7e6348dc
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-01-15 10:59:09 +0300

    feat: implements task status

---
+++ app/Http/Controllers/TaskStatusController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Http\Request;
+
+class TaskStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $taskStatuses = TaskStatus::all();
+        return view('task_statuses.index', compact('taskStatuses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $status = new TaskStatus();
+        return view('task_statuses.create', compact('status'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required|unique:task_statuses,name'
+        ]);
+
+        $taskStatus = new TaskStatus($data);
+        $taskStatus->save();
+        flash(__('messages.create_task_status'))->success();
+        return redirect()->route('task_statuses.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+//    public function show(string $id)
+//    {
+//        //
+//    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $status = TaskStatus::findORFail($id);
+        return view('task_statuses.edit', compact('status'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $status = TaskStatus::findORFail($id);
+        $data = $this->validate($request, [
+            'name' => 'required|unique:task_statuses,name,' . $status->id
+        ]);
+        $status->fill($data);
+        $status->save();
+        flash(__('messages.update_task_status'))->success();
+        return redirect()->route('task_statuses.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $status = TaskStatus::findOrFail($id);
+        $status->delete();
+        flash('Статус успешно удалён')->success();
+        return redirect()->route('task_statuses.index');
+    }
+}
---
+++ database/seeders/TaskStatusSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TaskStatus;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        TaskStatus::create(['name' => 'новый']);
+        TaskStatus::create(['name' => 'в работе']);
+        TaskStatus::create(['name' => 'на тестировании']);
+        TaskStatus::create(['name' => 'завершен']);
+    }
+}
