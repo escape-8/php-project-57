de50ee649703f25982d98e964ccb4f9fbb86588e
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-01-20 21:22:34 +0300

    refactor: change error view for label form

--- resources/views/label/form.blade.php
+++ resources/views/label/form.blade.php
@@ -1,19 +1,14 @@
-@if ($errors->any())
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
     <div>
         {{ Form::label('name', __('views.label.form.name')) }}
     </div>
     <div class="mt-2">
         {{ Form::text('name', $label->name ?? null, ['class' => 'rounded border-gray-300 w-1/3', 'id' => 'name']) }}
     </div>
+    @if ($errors->get('name'))
+        @foreach ($errors->get('name') as $error)
+            <div class="text-rose-600">{{ $error }}</div>
+        @endforeach
+    @endif
     <div class="mt-2">
         {{ Form::label('description', __('views.label.form.description')) }}
     </div>
