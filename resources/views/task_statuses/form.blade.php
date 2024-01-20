e556675c65297f5b4c424bc0622e658788e461d7
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-01-20 16:41:50 +0300

    refactor: change error view for task statuses form

--- resources/views/task_statuses/form.blade.php
+++ resources/views/task_statuses/form.blade.php
@@ -1,13 +1,3 @@
-@if ($errors->any())
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
 <div class="flex flex-col">
     <div>
         {{ Form::label('name', __('views.task_statuses.form.name')) }}
@@ -16,3 +6,8 @@
 <div class="mt-2">
     {{ Form::text('name', $status->name ?? '', ['class' =>'rounded border-gray-300 w-1/3']) }}
 </div>
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        <div class="text-rose-600">{{ $error }}</div>
+    @endforeach
+@endif
